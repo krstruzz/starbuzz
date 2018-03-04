ad11b9cb7de933a47de3b17b442540d8d89286f9
Keith Struzzieri <keith_struzzieri@yahoo.com>
2018-03-03 23:28:19 -0500

    update main menu with Drinks intent

--- app/src/main/java/com/struzzwebsolutions/starbuzz/TopLevelActivity.kt
+++ app/src/main/java/com/struzzwebsolutions/starbuzz/TopLevelActivity.kt
@@ -1,12 +1,30 @@
 package com.struzzwebsolutions.starbuzz
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.Toast
 
 class TopLevelActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_top_level)
+
+        val listView = findViewById<ListView>(R.id.list_options)
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, i, l ->
+            var intent: Intent? = null
+            when (i) {
+                0 -> intent = Intent(this, DrinkCategoryActivity::class.java)
+                1 -> Toast.makeText(this, "Not yet implemented", Toast.LENGTH_SHORT).show()
+                2 -> Toast.makeText(this, "Not yet implemented", Toast.LENGTH_LONG).show()
+            }
+            if (intent != null) startActivity(intent) else Toast.makeText(this, "Please try again", Toast.LENGTH_LONG).show()
+            //startActivity(intent)
+        }
     }
+
 }
